ebc9727d8bb9b5c0b80156cbbf957c705dc9bc6b
jlee720 <leejl950515@gmail.com>
2019-09-26 00:33:33 -0400

    Sort the data by year

--- graphql/api.js
+++ graphql/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = "https://yts.am/api/v2"
-const LIST_MOVIES = `${BASE_URL}/list_movies.json`
+const LIST_MOVIES = `${BASE_URL}/list_movies.json?sort_by=year`
 
 export const getMovies = async (limit, rating) => {
   try {
